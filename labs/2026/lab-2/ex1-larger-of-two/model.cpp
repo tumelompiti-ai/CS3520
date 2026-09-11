1dfaf572821583332d57e948f23a0a7c4f384e8b
unknown <tumelompiti360@gmail.com>
2026-09-11 10:29:39 +0200

    ex1:add C++ model for larger of two integers

---
+++ labs/2026/lab-2/ex1-larger-of-two/model.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a = 15, b = 9;
+    if (a >= b)
+        cout << a << endl;
+    else
+        cout << b << endl;
+    return 0;
+}
